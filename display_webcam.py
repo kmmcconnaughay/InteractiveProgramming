4b2d070472d919b816fb478636468f4ba5b62caf
Kerry McConnaughay <kmmcconnaughay@gmail.com>
2017-03-09 00:06:01 -0500

    Old functional version of webcam only code

--- display_webcam.py
+++ display_webcam.py
@@ -1,11 +1,12 @@
 import numpy
+import pylab
 import pygame
 import pygame.camera
 from pygame.locals import *
 
 DEVICE = '/dev/video0'
 SIZE = (640, 480)
-FILENAME = 'capture.png'
+
 
 def camstream():
     pygame.init()
@@ -19,23 +20,42 @@ def camstream():
         screen = camera.get_image(screen)
         display.blit(screen, (0,0))
         pygame.display.flip()
-        pygame.transform.flip(screen, True, False)
-        # pygame.time.wait(100) # Pauses program for a given number of ms
         for event in pygame.event.get():
             if event.type == QUIT:
                 capture = False
-            elif event.type == KEYDOWN and event.key == K_s:
-                pygame.image.save(screen, FILENAME)
-    camera.stop()
-    pygame.quit()
+                camera.stop()
+                pygame.quit()
+            if event.type == KEYDOWN:
+                # s for stops
+                if event.key == K_s:
+                    picture = 'stop'
+                    pygame.image.save(screen, "image_" + picture + ".jpg")
+                # p for play
+                if event.key == K_p:
+                    picture = 'play'
+                    pygame.image.save(screen, "image_" + picture + ".jpg")
+                # f for fast forward
+                if event.key == K_f:
+                    picture = 'fastforward'
+                    pygame.image.save(screen, "image_" + picture + ".jpg")
+                # r for restart
+                if event.key == K_r:
+                    picture = 'restart'
+                    pygame.image.save(screen, "image_" + picture + ".jpg")
     return
 
-def grayscale(self, img):
-    arr = pygame.surfarray.array3d(img)
-    #luminosity filter
-    avgs = [[(r*0.298 + g*0.587 + b*0.114) for (r,g,b) in col] for col in arr]
-    arr = numpy.array([[[avg,avg,avg] for avg in col] for col in avgs])
+def grayscale():
+    filenames = ['image_fastforward.jpg', 'image_play.jpg', 'image_restart.jpg',
+                 'image_stop.jpg']
+    for filename in filenames:
+        image = pygame.image.load(filename)
+        arr = pygame.surfarray.array3d(image)
+        #luminosity filter
+        avgs = [[(r*0.298 + g*0.587 + b*0.114) for (r,g,b) in col] for col in arr]
+        arr = numpy.array([[[avg,avg,avg] for avg in col] for col in avgs])
+        print(arr)
     return pygame.surfarray.make_surface(arr)
 
 if __name__ == '__main__':
     camstream()
+    grayscale()
